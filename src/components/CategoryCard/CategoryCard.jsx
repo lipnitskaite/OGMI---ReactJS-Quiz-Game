ec0cc50f5b3dfed27bcc9abd54ea637cf0423f61
Karolina Lipnitskaite <lipnitskaite@gmail.com>
2023-06-07 15:36:22 +0200

    feat: categories section layout is ready

--- src/components/CategoryCard/CategoryCard.jsx
+++ src/components/CategoryCard/CategoryCard.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import Style from './CategoryCard.module.scss';
 
-import harryPotterImage from '../../images/shayna-douglas--EwOTtOcReM-unsplash.jpg';
-
-function CategoryCard() {
+function CategoryCard({ categoryName, categoryImage }) {
   return (
     <li className={Style.card}>
-      <h4 className={Style.title}>Harry Potter</h4>
-      <img
-        className={Style.image}
-        src={harryPotterImage}
-        alt="Harry Potter Book"
-      />
+      <h4 className={Style.title}>{categoryName}</h4>
+      <img className={Style.image} src={categoryImage} alt={categoryName} />
     </li>
   );
 }
+
+CategoryCard.propTypes = {
+  categoryName: PropTypes.string.isRequired,
+  categoryImage: PropTypes.string.isRequired,
+};
 
 export default CategoryCard;
--- src/components/StartingPage/StartingPage.jsx
+++ src/components/StartingPage/StartingPage.jsx
@@ -1,15 +1,23 @@
 import React from 'react';
 
 import Style from './StartingPage.module.scss';
+import categories from '../../utils/data';
 import CategoryCard from '../CategoryCard/CategoryCard';
 
 function StartingPage() {
   return (
-    <div className={Style.startingPage}>
+    <section className={Style.startingPage}>
       <h2 className={Style.title}>Let&apos;s play!</h2>
       <p className={Style.subtitle}>Please choose your category</p>
-      <CategoryCard />
-    </div>
+      <ul className={Style.categories}>
+        {categories.map((category) => (
+          <CategoryCard
+            categoryName={category.title}
+            categoryImage={category.image}
+          />
+        ))}
+      </ul>
+    </section>
   );
 }
 
