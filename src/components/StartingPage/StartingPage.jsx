d2b6ebc79252e51b1373e5918ee7e7ff34e0254b
Karolina Lipnitskaite <lipnitskaite@gmail.com>
2023-06-07 13:37:26 +0200

    feat: basic styles of starting page are added

--- src/components/StartingPage/StartingPage.jsx
+++ src/components/StartingPage/StartingPage.jsx
@@ -6,8 +6,8 @@ import CategoryCard from '../CategoryCard/CategoryCard';
 function StartingPage() {
   return (
     <div className={Style.startingPage}>
-      <h2>Let&apos;s play!</h2>
-      <h3>Please choose your category</h3>
+      <h2 className={Style.title}>Let&apos;s play!</h2>
+      <p className={Style.subtitle}>Please choose your category</p>
       <CategoryCard />
     </div>
   );
