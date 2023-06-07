eb8debe4b3575391cc237f893a785cfa4bb6313b
Karolina Lipnitskaite <lipnitskaite@gmail.com>
2023-06-07 11:35:11 +0200

    linters are installed

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Style from './App.module.scss';
+
+function App() {
+  return (
+    <div className={Style.App}>
+      <header className={Style.Appheader}>
+        <p1>Initial project</p1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
